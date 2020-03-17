8bd8522af5e00cdeb94fbc761d48017dd23d74ea
hongweiliu-a <hongwei.liu@shopee.com>
2020-03-17 10:45:38 +0800

    commit

---
+++ Example/Pods/Target Support Files/Pods-WYLib_Tests/Pods-WYLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WYLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WYLib_Tests
+@end
---
+++ Example/Pods/Target Support Files/WYLib/WYLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WYLib : NSObject
+@end
+@implementation PodsDummy_WYLib
+@end
